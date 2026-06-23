f4e80ea2c6f6f42fea13e2dedac48398337fbc06
Viktor Stanislavov Minchev <61328078+viktorinkov@users.noreply.github.com>
2026-06-23 11:25:18 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        map1 = {}
+        map2 = {}
+        for char in s:
+            map1[char] = map1.get(char, 0) + 1
+
+        for char in t:
+            map2[char] = map2.get(char, 0) + 1
+
+        return map1 == map2
